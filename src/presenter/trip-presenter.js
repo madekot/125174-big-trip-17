6bf059bd44bd8d07d245f5d800f480452219ff9c
Mikhail Golubtsov <mgolubcov@gmail.com>
2022-05-03 14:56:50 +0300

    добавляет возможность открывать и закрывать точку маршрута + сообщение если точек 0

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -2,39 +2,86 @@ import FormEditView from '../view/form-edit-view';
 import RoutePointView from '../view/route-point-view';
 import SortingView from '../view/sorting-view';
 import TripListView from '../view/trip-list-view';
+import NoTripView from '../view/no-trip-view';
 
 import {render} from '../render';
-
 export default class TripsPresenter {
-  #container = null;
+  #boardContainer = null;
   #tripsModel = null;
-  #dataTrips = null;
-  #dataOffers = null;
 
   #tripListComponent = new TripListView();
 
-  init = (container, tripsModel) => {
-    this.#container = container;
+  #dataTrips = [];
+  #dataOffers = [];
+
+  constructor(boardContainer, tripsModel) {
+    this.#boardContainer = boardContainer;
     this.#tripsModel = tripsModel;
+  }
+
+  init = () => {
     this.#dataTrips = [...this.#tripsModel.trips];
     this.#dataOffers = [...this.#tripsModel.offers];
 
-    render(new SortingView(), this.#container);
-    render(this.#tripListComponent, this.#container);
-    render(
-      new FormEditView(
-        this.#dataTrips[0], this.#dataOffers[0]
-      ),
-      this.#tripListComponent.element
-    );
+    this.#renderBoard();
+  };
 
-    for (let i = 1; i < this.#dataTrips.length; i++) {
-      render(
-        new RoutePointView(
-          this.#dataTrips[i], this.#dataOffers[i]
-        ),
-        this.#tripListComponent.element
+  #renderTrip = (point, offers) => {
+    const formEditComponent = new FormEditView(point, offers);
+    const pointComponent = new RoutePointView(point, offers);
+
+    const replaceFormToPoint = () => {
+      this.#tripListComponent.element.replaceChild(
+        pointComponent.element, formEditComponent.element
       );
+    };
+
+    const replacePointToForm = () => {
+      this.#tripListComponent.element.replaceChild(
+        formEditComponent.element, pointComponent.element
+      );
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    formEditComponent.element.querySelector('form')
+      .addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      });
+
+    formEditComponent.element.querySelector('.event__rollup-btn')
+      .addEventListener('click', () => {
+        replaceFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      });
+
+    pointComponent.element.querySelector('.event__rollup-btn')
+      .addEventListener('click', () => {
+        replacePointToForm();
+        document.addEventListener('keydown', onEscKeyDown);
+      });
+
+    render(pointComponent, this.#tripListComponent.element);
+  };
+
+  #renderBoard = () => {
+    render(new SortingView(), this.#boardContainer);
+    render(this.#tripListComponent, this.#boardContainer);
+
+    if (this.#dataTrips.length) {
+      for (let i = 0; i < this.#dataTrips.length; i++) {
+        this.#renderTrip(this.#dataTrips[i], this.#dataOffers[i]);
+      }
+    } else {
+      render(new NoTripView(), this.#boardContainer);
     }
   };
 }
