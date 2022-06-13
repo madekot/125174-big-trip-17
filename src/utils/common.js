9dd6da5e0066dae5be269e61f4e078f5aed5704f
Mikhail Golubtsov <mgolubcov@gmail.com>
2022-06-13 11:57:10 +0300

    удаляет не использованные переменные

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,33 +7,11 @@ import {
   HOURS_IN_DAY,
 } from '../const';
 
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
-const getRandomValueArray = (arr) => arr[getRandomInteger(0, arr.length - 1)];
-
-const makeCounter = (options = {}) => {
-  let start = options.start || 0;
-  return () => ++start;
-};
-
 const transformFirstLetterWordUppercase = (str) => (
   str[0].toUpperCase() + str.slice(1)
 );
 
 const getTextFinalSay = (str) => str.split(' ').pop();
-
-const getRandomBoolean = () => Math.random() < 0.5;
-
-const getRandomDayStart = (time = dayjs()) => (
-  getRandomBoolean()
-    ? time.subtract(getRandomBoolean(), 'day')
-    : time.add(getRandomBoolean(), 'day')
-);
 
 const getHours = (time) => Math.trunc(
   time
@@ -72,12 +50,7 @@ export {
   getDays,
   getDifferenceMilliseconds,
   getHours,
-  getRandomBoolean,
-  getRandomDayStart,
-  getRandomInteger,
-  getRandomValueArray,
   getTextFinalSay,
   isDoubleDigits,
-  makeCounter,
   transformFirstLetterWordUppercase,
 };
